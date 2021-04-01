b5997cd5d4eadf5e8b40cdcbe9aa2779692a0eaf
ds <1159629515@qq.com>
2021-04-01 14:40:03 +0800

    ds

---
+++ basic/src/com/ds/basic/oop/Son.java
@@ -0,0 +1,16 @@
+package com.ds.basic.oop;
+
+/**
+ * @Author ds
+ * @Date 2021/3/29 9:34
+ * @Description
+ */
+public class Son implements Father{
+
+    public int i = 10;
+
+    @Override
+    public void print() {
+        System.out.println(i);
+    }
+}
