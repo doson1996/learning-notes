a148919e1331362175345614c91eac549ec82430
ds <1159629515@qq.com>
2021-07-08 23:34:12 +0800

    ds

---
+++ jvm/src/com/ds/jvm/chapter01/Demo20LogTest.java
@@ -0,0 +1,12 @@
+package com.ds.jvm.chapter01;
+
+/**
+ * -XX:+PrintGCDetails
+ * @author ds
+ * @date 2021/7/8 23:30
+ */
+public class Demo20LogTest {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
