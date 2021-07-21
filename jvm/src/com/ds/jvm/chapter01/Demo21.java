a4eba45468c3a246727589e6b4390220d30fcca0
ds <1159629515@qq.com>
2021-07-21 22:34:56 +0800

    ds

---
+++ jvm/src/com/ds/jvm/chapter01/Demo21.java
@@ -0,0 +1,18 @@
+package com.ds.jvm.chapter01;
+
+/**
+ * @author ds
+ * @date 2021/7/21 22:33
+ */
+public class Demo21 {
+    public static void main(String[] args) {
+        Integer i1 = 70;
+        Integer i2 = 70;
+
+        Integer i3 = 128;
+        Integer i4 = 128;
+
+        System.out.println(i1 == i2);
+        System.out.println(i3 == i4);
+    }
+}
