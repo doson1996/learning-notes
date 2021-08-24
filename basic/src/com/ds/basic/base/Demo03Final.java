e3002723b4214741d64b3f87206700885ac9335e
ds <1159629515@qq.com>
2021-08-24 22:36:23 +0800

    ds

---
+++ basic/src/com/ds/basic/base/Demo03Final.java
@@ -0,0 +1,18 @@
+package com.ds.basic.base;
+
+/**
+ * @author ds
+ * @date 2021/8/24 22:32
+ */
+public class Demo03Final {
+
+    public static final User ZS = new User("zs", 20);
+
+    public static void main(String[] args) {
+        /**
+         * Cannot assign a value to final variable 'ZS'
+         * 无法为最终变量“ZS”赋值
+         */
+     //  ZS = new User("ls", 21);
+    }
+}
