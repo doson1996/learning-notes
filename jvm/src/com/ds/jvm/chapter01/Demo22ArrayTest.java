71fb0c243f6f5c8e85d7f5f900dbd7822ac0392d
ds <1159629515@qq.com>
2021-09-01 22:16:58 +0800

    ds

---
+++ jvm/src/com/ds/jvm/chapter01/Demo22ArrayTest.java
@@ -0,0 +1,18 @@
+package com.ds.jvm.chapter01;
+
+/**
+ * @author ds
+ * @date 2021/9/1 22:15
+ */
+public class Demo22ArrayTest {
+    public static void main(String[] args) {
+        int[] arr1 = new int[10];
+        System.out.println("arr1 = " + arr1);
+
+        String[] arr2 = new String[10];
+        System.out.println("arr2 = " + arr2);
+
+        long[][] arr3 = new long[10][10];
+        System.out.println("arr3 = " + arr3);
+    }
+}
