ebff91665784ebc507ad3e2b8e4facb4fc298853
ds <1159629515@qq.com>
2021-09-14 22:14:31 +0800

    ds

---
+++ jvm/src/com/ds/jvm/chapter01/Demo23ClassLoader.java
@@ -0,0 +1,31 @@
+package com.ds.jvm.chapter01;
+
+/**
+ * @author ds
+ * @date 2021/9/14 22:08
+ */
+public class Demo23ClassLoader {
+    public static void main(String[] args) {
+
+        //系统类加载器 sun.misc.Launcher$AppClassLoader@18b4aac2
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println("systemClassLoader = " + systemClassLoader);
+
+        //扩展类加载器 sun.misc.Launcher$ExtClassLoader@1b6d3586
+        ClassLoader extClassLoader = systemClassLoader.getParent();
+        System.out.println("extClassLoader = " + extClassLoader);
+
+        //引导类加载器 null
+        ClassLoader bootstrapClassLoader = extClassLoader.getParent();
+        System.out.println("bootstrapClassLoader = " + bootstrapClassLoader);
+
+        //引用类型数组根据数组类型的类加载器   String是由引导类加载器加载，所以为null
+        String[] strArr = new String[10];
+        System.out.println("strArr = " + strArr.getClass().getClassLoader());
+
+        //基本类型数组不需要类加载器加载，所以为null
+        int[] intArr = new int[10];
+        System.out.println("intArr = " + intArr.getClass().getClassLoader());
+    }
+
+}
