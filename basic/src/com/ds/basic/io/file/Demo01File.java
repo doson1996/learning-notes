04d814c520eca1298ed85fa112432218c776a0f4
ds <1159629515@qq.com>
2022-03-17 23:46:42 +0800

    deleteOnExit

---
+++ basic/src/com/ds/basic/io/file/Demo01File.java
@@ -0,0 +1,24 @@
+package com.ds.basic.io.file;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * delete无需解释，为直接删除，
+ * deleteOnExit文档解释为：在虚拟机终止时，请求删除此抽象路径名表示的文件或目录。
+ * 也就是说，程序运行deleteOnExit成功后，File并没有直接删除，而是在虚拟机正常运行结束后才会删除。
+ * @author ds
+ * @date 2022/3/17 23:05
+ */
+public class Demo01File {
+    public static void main(String[] args) throws IOException {
+        File file = new File("a.txt");
+        System.out.println(file.getAbsolutePath());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        file.deleteOnExit();
+    }
+}
