92853a45fd9dc20dc3c4e18e4acdff134c44ecf1
ds <1159629515@qq.com>
2022-06-12 23:37:48 +0800

    ds

---
+++ basic/src/com/ds/basic/reflect/demo02/Test.java
@@ -0,0 +1,18 @@
+package com.ds.basic.reflect.demo02;
+
+import java.lang.reflect.Field;
+
+/**
+ * 通过反射获取字段值
+ * @author ds
+ */
+public class Test {
+    public static void main(String[] args) throws Exception {
+        User user = new User("张三", 20);
+        Field[] fields = User.class.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            System.out.println(field.getName() + "=" + field.get(user));
+        }
+    }
+}
