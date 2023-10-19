87af420a3918d948f2209d0ab9d822426cf55ece
ds <1159629515@qq.com>
2023-10-20 00:02:14 +0800

    ds

--- springframework/src/main/java/com/ds/springframework/chapte01/bean/Demo02FactoryBean.java
+++ springframework/src/main/java/com/ds/springframework/chapte01/bean/Demo02FactoryBean.java
@@ -1,8 +1,5 @@
 package com.ds.springframework.chapte01.bean;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-
 /**
  * FactoryBean {@link org.springframework.beans.factory.FactoryBean}
  * 也是创建对象，但是没有标准流程
