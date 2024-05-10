baaba2899886f5c0e6862f950f4570a04bcabab8
ds <1159629515@qq.com>
2024-05-10 17:55:20 +0800

    ds

--- work/haizhi/src/main/java/com/ds/Test.java
+++ work/haizhi/src/main/java/com/ds/Test.java
@@ -12,6 +12,7 @@ public class Test {
     public static void main(String[] args) {
         String q = "请描述重庆银行基本情况!";
         q = "重庆银行的主要业务范围是什么？";
+      //  System.out.println(HanLP.convertToPinyinString(q, "", false));
         System.out.println(HanLP.segment(q));
         System.out.println(StandardTokenizer.segment(q));
         System.out.println(HanLP.newSegment("感知机").seg(q));
