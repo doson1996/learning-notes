b8eea31a96a22a6d04d2d2242283d5ea6552e2cf
ds <1159629515@qq.com>
2024-09-20 11:15:17 +0800

    ds

--- graph/nebula/src/main/java/com/ds/graph/nebula/test/Test01.java
+++ graph/nebula/src/main/java/com/ds/graph/nebula/test/Test01.java
@@ -1,8 +1,13 @@
 package com.ds.graph.nebula.test;
 
+import com.vesoft.nebula.Edge;
+import com.vesoft.nebula.Row;
+import com.vesoft.nebula.Value;
+import com.vesoft.nebula.Vertex;
 import com.vesoft.nebula.client.graph.NebulaPoolConfig;
 import com.vesoft.nebula.client.graph.data.HostAddress;
 import com.vesoft.nebula.client.graph.data.ResultSet;
+import com.vesoft.nebula.client.graph.data.ValueWrapper;
 import com.vesoft.nebula.client.graph.exception.IOErrorException;
 import com.vesoft.nebula.client.graph.net.NebulaPool;
 import com.vesoft.nebula.client.graph.net.Session;
@@ -27,9 +32,34 @@ public class Test01 {
         Session session = pool.getSession("root", "1", false);
         ResultSet execute = session.execute("SHOW HOSTS;");
         System.out.println("execute = " + execute);
+        String nql = "GO FROM \"player102\" OVER * BIDIRECT YIELD edge AS e;";
+        nql = "GO 2 STEPS FROM \"player102\" OVER follow YIELD dst(edge);";
+        nql = "GO FROM \"player100\" OVER follow REVERSELY YIELD src(edge) as player;";
+        nql = " MATCH (v1:player{name: \"LeBron James\"}) <-[r:follow]-> (v2)\n" +
+                "      RETURN type(r) AS Type, v1,v2, r";
 
-        String s = session.executeJson("USE basketballplayer; MATCH (v:player) RETURN v;");
-        System.out.println("s = " + s);
+//        String s = session.executeJson("USE basketballplayer; MATCH (v:player) RETURN v;");
+        String result = session.executeJson("USE basketballplayer; " + nql);
+        System.out.println("result = " + result);
+
+        ResultSet r1 = session.execute("USE basketballplayer; " + nql);
+
+        List<Row> rows = r1.getRows();
+        int i = 0;
+        while (i < rows.size()) {
+            for (ValueWrapper value : r1.rowValues(i)) {
+                if (value.isEdge()) {
+                    System.out.println("E: " + value.asRelationship());
+                }
+
+                if (value.isVertex()) {
+                    System.out.println("V: " + value.asNode());
+                }
+            }
+            i++;
+
+        }
+
         session.release();
         pool.close();
     }
