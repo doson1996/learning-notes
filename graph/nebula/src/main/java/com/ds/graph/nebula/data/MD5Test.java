c7faf15b21cb16dbe130ce4dd44a04ec82fed6dc
ds <1159629515@qq.com>
2025-01-03 17:39:19 +0800

    ds

--- graph/nebula/src/main/java/com/ds/graph/nebula/data/MD5Test.java
+++ graph/nebula/src/main/java/com/ds/graph/nebula/data/MD5Test.java
@@ -10,7 +10,9 @@ import cn.hutool.crypto.SecureUtil;
 public class MD5Test {
     public static void main(String[] args) {
         String company = "915000002024110502重庆测试技术有限公司";
+        company = "915000002024110503疑似企业1";
+//        company = "915000002024110504疑似企业2";
         String s = SecureUtil.md5(company);
-        System.out.println("s = " + s);
+        System.out.println("s = " + s.toUpperCase());
     }
 }
