3c872a988cf52ee559ece0367ca6421c27454d9d
11596 <dusheng@stargraph.cn>
2025-02-25 17:56:28 +0800

    ds

--- spi/cache-disk/src/main/java/com/ds/cache/impl/DiskCache.java
+++ spi/cache-disk/src/main/java/com/ds/cache/impl/DiskCache.java
@@ -1,26 +1,62 @@
 package com.ds.cache.impl;
 
-import com.ds.cache.Cache;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.ConcurrentHashMap;
+
+import com.ds.cache.Cache;
 
 /**
  * @author ds
  */
 public class DiskCache implements Cache {
 
-    private static final ConcurrentHashMap<String,Object> cache = new ConcurrentHashMap<>(64);
-
     @Override
     public Object get(String key) {
-        return cache.get(key);
+        File cacheFile = getCacheFile(key);
+        try {
+            if (cacheFile.exists()) {
+                BufferedReader reader = new BufferedReader(new FileReader(cacheFile));
+                return reader.readLine();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 
     @Override
     public void set(String key, Object value) {
-        Path path = Paths.get("cache.txt");
-        cache.put(key, value);
+        File cacheFile = getCacheFile(key);
+        BufferedWriter writer = null;
+        try {
+            if (cacheFile.exists()) {
+                cacheFile.delete();
+            }
+            writer = new BufferedWriter(new FileWriter(cacheFile));
+            writer.write(String.valueOf(value));
+            writer.flush();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
+
+    private File getCacheFile(String key) {
+        String fileName = "cache-" + key + ".txt";
+        Path path = Paths.get(fileName);
+        return path.toFile();
+    }
+
 }
--- spi/cache-test/src/main/java/com/ds/cache/Test.java
+++ spi/cache-test/src/main/java/com/ds/cache/Test.java
@@ -2,6 +2,8 @@ package com.ds.cache;
 
 import java.util.ServiceLoader;
 
+import com.ds.cache.impl.DiskCache;
+
 /**
  * @author ds
  */
@@ -10,6 +12,11 @@ public class Test {
         ServiceLoader<Cache> caches = ServiceLoader.load(Cache.class);
         for (Cache c : caches) {
             System.out.println(c);
+            if (c.getClass() == DiskCache.class) {
+                c.set("key1", "value1");
+                Object v1 = c.get("key1");
+                System.out.println("v1 = " + v1);
+            }
         }
     }
 }
