a3f4c68a1b24c2a70c1f75bc3a5e68b412ff3bae
11596 <1@qq.com>
2026-03-24 09:25:26 +0800

    ds

---
+++ springframework/src/main/java/com/ds/springframework/chapter01/ylzr/Config.java
@@ -0,0 +1,14 @@
+package com.ds.springframework.chapter01.ylzr;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ds
+ * @date 2026/2/5
+ * @description
+ */
+@ComponentScan
+@Configuration
+public class Config {
+}
