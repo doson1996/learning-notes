b7db36d4afd3863f46e2dea6e905269e73490c54
ds <1159629515@qq.com>
2026-07-24 13:05:54 +0800

    ds

--- springframework/src/main/java/com/ds/springframework/chapter01/lookup/Test.java
+++ springframework/src/main/java/com/ds/springframework/chapter01/lookup/Test.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 /**
  * @author ds
  * @date 2024/4/16
- * @description
+ * @description 1.spring在创建bean时判断有没有@Lookup注解，有的话就把方法封装成LookupOverride加到BeanDefinition的MethodOverrides {@link org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor#determineCandidateConstructors(java.lang.Class, java.lang.String)}
+ *              2.在实例化时判断有没有MethodOverrides，有的话进行代理 {@link org.springframework.beans.factory.support.SimpleInstantiationStrategy#instantiate(org.springframework.beans.factory.support.RootBeanDefinition, java.lang.String, org.springframework.beans.factory.BeanFactory)}
  */
 public class Test {
     public static void main(String[] args) {
