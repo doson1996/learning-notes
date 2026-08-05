18d5d3f7bc4d4eaaea3ffb6357e6bcc875d59191
ds <1159629515@qq.com>
2026-08-05 11:57:53 +0800

    ds

---
+++ ai/src/main/java/com/ds/dashscope/Image2TextDemo.java
@@ -0,0 +1,37 @@
+package com.ds.dashscope;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversation;
+import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversationParam;
+import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversationResult;
+import com.alibaba.dashscope.common.MultiModalMessage;
+import com.alibaba.dashscope.common.Role;
+import com.alibaba.dashscope.utils.JsonUtils;
+
+/**
+ * @author ds
+ * @date 2026/8/5
+ * @description
+ */
+public class Image2TextDemo {
+    public static void main(String[] args) throws Exception {
+        MultiModalConversation conv = new MultiModalConversation();
+        MultiModalMessage userMessage = MultiModalMessage.builder().role(Role.USER.getValue())
+                .content(Arrays.asList(
+                        Collections.singletonMap("image", "https://dashscope.oss-cn-beijing.aliyuncs.com/images/dog_and_girl.jpeg"),
+                        Collections.singletonMap("image", "https://dashscope.oss-cn-beijing.aliyuncs.com/images/tiger.png"),
+                        Collections.singletonMap("image", "https://dashscope.oss-cn-beijing.aliyuncs.com/images/rabbit.png"),
+                        Collections.singletonMap("text", "这些是什么?"))).build();
+        MultiModalConversationParam param = MultiModalConversationParam.builder()
+                // 若没有配置环境变量，请用百炼API Key将下行替换为：.apiKey("sk-xxx")
+                .apiKey(System.getProperty("AI_API_KEY"))
+                // 此处以qwen-vl-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+                .model("qwen-vl-plus")
+                .message(userMessage)
+                .build();
+        MultiModalConversationResult result = conv.call(param);
+        System.out.println(JsonUtils.toJson(result));
+    }
+}
